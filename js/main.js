05956fdb510a240bf1318ec3310d914bba22a786
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-05-16 11:25:01 +0300

    add

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 $(function () {
 
-    $('.bike-slider').slick();
+    $('.bike-slider').slick({
+        arrows: false,
+        dots: true,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
 
 })
